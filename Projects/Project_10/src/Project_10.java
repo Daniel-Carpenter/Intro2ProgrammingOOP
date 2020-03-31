5e9dfbc3f821e191e029b40cb8ef8d0aaf3d7111
Daniel Carpenter <dbry1024@gmail.com>
2020-03-31 11:28:29 -0500

    Updating Project 10

--- Projects/Project_10/src/Project_10.java
+++ Projects/Project_10/src/Project_10.java
@@ -1,4 +1,21 @@
 
-public class Project_10 {
+public class Project_10 
+{
+	public static void main(String[] args)
+	{
+		// Construct Image Array -------------------------------
+			int row = 3;
+			int col = 2;
+			int[][] image = new int[row][col];
+			String filename = "image.txt";
+			
+		// Call Methods ----------------------------------------
+			saveImage(filename, image);
+	}
+	
+	public static void saveImage(String filename, int[][] image)
+	{
+		
+	}
 
 }

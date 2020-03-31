0b2a465e3f692667a7988cd17f2cc99c47e96ba5
Daniel Carpenter <dbry1024@gmail.com>
2020-03-31 17:51:52 -0500

    P10: Working on Diagonal Method

--- Projects/Project_10/src/createDiagonalStripes_Example.java
+++ Projects/Project_10/src/createDiagonalStripes_Example.java
@@ -6,7 +6,7 @@ public class createDiagonalStripes_Example
 	{
 		int height		= 4;
 		int width		= 4;
-		int stripeWidth	= 2;
+		int stripWidth	= 2;
 		
 		// METHOD BELOW ----------------------------------------------			
 			final int white 	= 0; // 255 <- grayscale
@@ -18,26 +18,25 @@ public class createDiagonalStripes_Example
 			// Create the Actual array
 			for (int row = 0; row < array.length; ++row)
 			{
-				// OUTER LOOP -------------------------------------------- // GO THROUGH LOOP TO MAKE DIAGONAL
-
-				// INNER LOOP --------------------------------------------
-					for (int col = 0; col < array[0].length; ++col)
-					{						
-						if ((col + row) % stripeWidth == 0)
-						{
-							if (color == white)
-							{
-								color = black;
-							}
-							else
-							{
-								color = white;
-							}
-						}
+				for (int col = 0; col < array[0].length; ++col)
+				{
 						
-						array[row][col] = color;
-
+					array[row][col] = col + row;
+					array[row][col] = (array[row][col] + 1) % (stripWidth + 1);
+					if (array[row][col] % stripWidth == 0)
+					{
+						if (color == white)
+						{
+							color = black;
+						}
+						else
+						{
+							color = white;
+						}
 					}
+					array[row][col] = color;
+				}
+				
 				System.out.println(Arrays.toString(array[row]));
 			}
 	}

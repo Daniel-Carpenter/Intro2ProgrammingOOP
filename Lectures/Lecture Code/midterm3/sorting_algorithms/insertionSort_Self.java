ea6a42c1419932b0ed1f09ddebe2fa80cd64ee2d
Daniel Carpenter <dbry1024@gmail.com>
2020-04-03 11:12:59 -0500

    Insertion Sort (In Class)

--- Lectures/Lecture Code/midterm3/sorting_algorithms/insertionSort_Self.java
+++ Lectures/Lecture Code/midterm3/sorting_algorithms/insertionSort_Self.java
@@ -1,5 +1,34 @@
 
 public class insertionSort_Self 
 {
+	public static void main(String[] args)
+	{
+		int[] array = {4, 7, 2, 5, 1, 3, 8, 0, 0, 0};
+		int arraySize = array.length;
+		
+	}
+	
+	public static void insertionSort(int[] array)
+	{
+		int start = 0;
+		int aux = 0;
+		
+		for (start = 1; start < array.length; ++start);
+		{
+			//Move value to aux
+			aux = array[start];
+		}
+		
+		
+		// shift values that are larhet than the value in the aux
+		for (int inner = start -1; inner >= 0 && array[inner] > aux; --inner)
+		{
+			//shift
+			array[inner + 1] = array[inner];
+		}
+		
+		// Shift aux back to the array
+		array[start] = aux;
+	}
 
 }

b25214dec10e134c4e98e4e155a7761a75eb5c7f
Daniel Carpenter <dbry1024@gmail.com>
2020-04-07 12:42:23 -0500

    Finished Pac Man Graphic

--- Projects/Project_11/src/Project_11.java
+++ Projects/Project_11/src/Project_11.java
@@ -2,6 +2,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.lang.Object;
 
 public class Project_11 extends JPanel
 {
@@ -9,6 +10,15 @@ public class Project_11 extends JPanel
 		private static final int WIDTH = 640;
 		private static final int HEIGHT = 360;
 		private static final String WINDOW_TITLE = "Project 11: Image Output for Java Art";
+		
+		// LIST OF COLORS
+			Color red 		= new Color(96, 45, 45);
+			Color blue 		= new Color(38, 66, 96);
+			Color green		= new Color(33, 73, 62);
+			Color yellow 	= new Color(169, 123, 25);
+			Color purple	= new Color(86, 59, 93);
+			Color orange	= new Color(255, 170, 128);
+			Color pacMan	= new Color(255,238,0);
 	
 		
 	// MAIN METHOD  --------------------------------------------------------------------------------------------------------
@@ -27,19 +37,11 @@ public class Project_11 extends JPanel
 	
 	// CREATE PICTURE  -----------------------------------------------------------------------------------------------------	
 		public void paintComponent(Graphics g)
-		{
-			// LIST OF COLORS
-				Color red 		= new Color(96, 45, 45);
-				Color blue 		= new Color(38, 66, 96);
-				Color green		= new Color(33, 73, 62);
-				Color yellow 	= new Color(169, 123, 25);
-				Color purple	= new Color(86, 59, 93);
-				Color orange	= new Color(255, 170, 128);
-			
+		{	
 			// Do not change these two lines.
 				super.paintComponent(g);
 				Graphics2D g2d = (Graphics2D) g;
-			
+				
 			// HORIZONTAL LINE
 				Line2D.Double line = new Line2D.Double(0, HEIGHT/2, WIDTH, HEIGHT/2);
 				g2d.setColor(purple);
@@ -49,5 +51,28 @@ public class Project_11 extends JPanel
 				line.setLine(WIDTH/2, 0, WIDTH/2, HEIGHT);
 				g2d.setColor(green);
 				g2d.draw(line);
+				
+			// CREATE ARC
+				// DEFINE INPUTS
+					double x 		= WIDTH / 20;
+					double y 		= HEIGHT / 4.5;
+					double w 		= 200;
+					double h		= 200;
+					double angSt 	= 35;
+					double angExt	= 305;
+					int  closure	= Arc2D.PIE;
+					
+					final float dash1[] = {10.0f};
+					BasicStroke dashed = new BasicStroke(1.0f,
+	                        BasicStroke.CAP_BUTT,
+	                        BasicStroke.JOIN_MITER,
+	                        10.0f, dash1, 0.0f);
+					
+				// CREATE PAC MAN
+					Arc2D.Double arc = new Arc2D.Double(x, y, w, h, angSt, angExt, closure);
+					g2d.setPaint(pacMan);
+					g2d.setStroke(dashed);
+					g2d.draw(arc);
+					g2d.fill (arc);
 		}
 }

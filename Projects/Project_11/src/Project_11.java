2d319d9355007c04544fc2fd2c57812a97cc42fc
Daniel Carpenter <dbry1024@gmail.com>
2020-04-07 11:37:14 -0500

    Updating P10

--- Projects/Project_11/src/Project_11.java
+++ Projects/Project_11/src/Project_11.java
@@ -5,41 +5,41 @@ import java.awt.geom.*;
 
 public class Project_11 extends JPanel
 {
-	// Change these constants to adjust the size and title of the window.
-	private static final int WIDTH = 640;
-	private static final int HEIGHT = 480;
-	private static final String WINDOW_TITLE = "My Image Title!";
+	// STATIC CONSTANTS ----------------------------------------------------------------------------------------------------
+		private static final int WIDTH = 640;
+		private static final int HEIGHT = 480;
+		private static final String WINDOW_TITLE = "My Image Title!";
 	
-	public static void main(String[] args)
-	{
-		JPanel panel = new Project_11();
-		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		
-		JFrame frame = new JFrame(WINDOW_TITLE);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.add(panel);
-		frame.pack();
-	}
+	// MAIN METHOD  --------------------------------------------------------------------------------------------------------
+		public static void main(String[] args)
+		{
+			JPanel panel = new Project_11();
+			panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+			
+			JFrame frame = new JFrame(WINDOW_TITLE);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+			frame.add(panel);
+			frame.pack();
+		}
+
 	
-	
-	// Add your drawing code to this method.
-	public void paintComponent(Graphics g)
+	// CREATE PICTURE  -----------------------------------------------------------------------------------------------------	
+		public void paintComponent(Graphics g)
 	{
 		// Do not change these two lines.
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 		
-		// Replace the example below with your code.
-		
 		// Draw a horizontal, blue line in the middle of the screen.
 		Line2D.Double line = new Line2D.Double(0, HEIGHT/2, WIDTH, HEIGHT/2);
-		g2d.setColor(Color.BLUE);
+		g2d.setColor(Color.BLACK);
 		g2d.draw(line);
 		
-		// Draw a vertical, red line in the middle of the screen.
+		// VERTICAL
 		line.setLine(WIDTH/2, 0, WIDTH/2, HEIGHT);
-		g2d.setColor(Color.RED);
+		g2d.setColor(Color.ORANGE);
 		g2d.draw(line);
 	}
 }

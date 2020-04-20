a1c09e3790ebdcfa36c8ae93790e16b87640ba9c
Daniel Carpenter <dbry1024@gmail.com>
2020-04-20 09:59:18 -0500

    General Update

---
+++ Past Exams/Test_Prep/src/SelectionSort.java
@@ -0,0 +1,28 @@
+
+public class SelectionSort 
+{
+	public static void main (String args)
+	{
+		int[] array = {4, 7, 2, 5, 1, 3, 8, 6, 7};
+		int size = array.length;
+		
+		
+	}
+	
+	public static int[] selectionSort(int array, int size)
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			
+		}
+	}
+	
+	public static int minIndex(int[] array, int size)
+	{
+		int min = array[0]
+		for (int i = min + 1; i < size; ++i)
+		{
+			if (array[min] )
+		}
+	}
+}

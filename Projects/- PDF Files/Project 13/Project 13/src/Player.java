50a1eacd4a7a084ff7aeff8784ae01c5327d079f
Daniel Carpenter <dbry1024@gmail.com>
2020-04-28 10:27:56 -0500

    Start Project 13

---
+++ Projects/- PDF Files/Project 13/Project 13/src/Player.java
@@ -0,0 +1,72 @@
+/** @author James Dizikes
+ *  Modified by Deborah Trytten use use the Deck class instead of an ArrayList.
+ *  Javadoc added 4/20/2020.
+ * CS 1324 Fall 2019
+ */
+
+public class Player
+{
+	
+	/** Construct a Player object with the given name and an empty hand.
+	 * 
+	 * @param name The Player's name.
+	 */
+	public Player(String name)
+	{
+
+	}
+	
+	/** Access the Player's name.
+	 * 
+	 * @return The Player's name.
+	 */
+	public String getName()
+	{
+		return null; // change this
+	}
+	
+	/** Return the number of cards in the Player's current hand.
+	 * 
+	 * @return The number of cards in the Player's current hand.
+	 */
+	public int getHandSize()
+	{
+		return 0; // change this
+	}
+
+	/** Add an additional card into the Player's hand. 
+	 * @param card The card to be added to the Player's hand.
+	 */
+	public void takeCard(Card card)
+	{
+		
+	}
+	
+	/** Remove a given card from the Player's hand.  If the card isn't in the hand,
+	 * nothing is done.
+	 * @param card The card to be removed from the Player's hand.
+	 */
+	public void removeCard(Card card)
+	{
+		
+	}
+	
+	/** Remove the top card from a Player's hand and return it.
+	 * 
+	 * @return The top card in the player's hand.
+	 */
+	public Card playCard()
+	{
+		return null; // change this
+	}
+	
+	/** Create a String containing data attached to the Player. The format is as follows:
+	 * name: cards (comma separated).
+	 * 
+	 * @return A string containing the Player's name and hand.
+	 */
+	public String toString()
+	{
+		return null;
+	}
+}
---
+++ Projects/Project_13/src/Project_13.java
@@ -0,0 +1,10 @@
+// Daniel Carpenter
+
+
+public class Project_13 
+{
+	public static void main (String[] args)
+	{
+		
+	}
+}
